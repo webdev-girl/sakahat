a26c721524b5964e8551c42d47fde61f80b464d5
Sakac934 <sakac934@msn.com>
2019-05-24 11:39:20 -0600

    file upload test form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,7 +33,7 @@ require('vendor/autoload.php');
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
 $s3 = new Aws\S3\S3Client([
     'version'  => '2006-03-01',
-    'region'   => 'us-east-1',
+    'region'   => 'ca-central-1',
 ]);
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 ?>
